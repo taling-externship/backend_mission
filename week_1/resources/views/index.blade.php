5d77574de57cfcebfd459335cde86d2811f43b17
sunday00 <kind65505256@gmail.com>
2022-02-09 01:29:32 +0900

    prepare main

---
+++ week_1/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('content')
+<main class="container mx-auto px-4 my-4">
+    <header>
+        <h1 class="text-4xl">
+            <a href="/">Hello laravel</a>
+        </h1>
+    </header>
+
+
+</main>
+@endsection
+
