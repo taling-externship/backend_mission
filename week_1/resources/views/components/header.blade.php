4249480c317739135a81245c990b56720ab6535e
sunday00 <kind65505256@gmail.com>
2022-02-09 01:39:37 +0900

    write my working note on readme.md

---
+++ week_1/resources/views/components/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <h1 class="text-4xl">
+        <a href="/">Hello laravel</a>
+    </h1>
+</header>
--- week_1/resources/views/index.blade.php
+++ week_1/resources/views/index.blade.php
@@ -2,11 +2,7 @@
 
 @section('content')
 <main class="container mx-auto px-4 my-4">
-    <header>
-        <h1 class="text-4xl">
-            <a href="/">Hello laravel</a>
-        </h1>
-    </header>
+    <x-header />
 
 
 </main>
