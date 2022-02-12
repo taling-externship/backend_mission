62c30fc73ef852b873ad8d8ea4a164463181bddd
Kyungseo-Park <troublesome.dev@gmail.com>
2022-02-13 03:30:44 +0900

    feat(migration): Board, Image 데이터베이스 migration 파일 추가

---
+++ week_1/database/migrations/2022_02_12_171758_create_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('board_id')->constrained('boards');
+            $table->string('alt')->comment('파일 이름');
+            $table->string('src')->comment('파일 경로');
+            $table->unsignedInteger('order')->comment('이미지 순서');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
