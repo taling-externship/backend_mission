ae6c9ee701ca4c5b6cd2d8aa5e3e468a9618444d
Kyungseo-Park <troublesome.dev@gmail.com>
2022-02-13 03:30:44 +0900

    feat(migration): Board, Image 데이터베이스 migration 파일 추가

---
+++ week_1/database/migrations/2022_02_12_162354_create_boards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id();
+            $table->string("title")->comment("제목");
+            $table->text("body")->comment("내용");
+            $table->string("slug")->unique()->comment("제목-Slug");
+            $table->boolean('is_show')->default(true)->comment("공개 여부");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
