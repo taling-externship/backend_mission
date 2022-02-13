2c47417e5c35325ba48480f6d2b79a922e09e590
Kyungseo-Park <troublesome.dev@gmail.com>
2022-02-13 18:48:10 +0900

    feat(View-layout): Layout 추가 && view/Components 삭제

--- week_1/resources/views/home.blade.php
+++ week_1/resources/views/home.blade.php
@@ -1,11 +1,4 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
+@extends('layout.default')
+@section('content')
 
-</body>
-</html>
+@stop
