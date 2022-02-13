29a41332be7641d3586e70a7f640468e968d149c
Kyungseo-Park <troublesome.dev@gmail.com>
2022-02-13 18:54:46 +0900

    feat(Request): BoardController에 유효성 검사와 Default 데이터를 위한 Request 추가

---
+++ week_1/app/Http/Requests/Board/BoardUpdateRequest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Requests\Board;
+
+use Illuminate\Foundation\Http\FormRequest;
+use JetBrains\PhpStorm\ArrayShape;
+
+class BoardUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    #[ArrayShape([
+        "id" => "string",
+        "title" => "string",
+        "body" => "string",
+        "slug_id" => "string",
+        "slug" => "string"
+    ])] public function rules(): array
+    {
+        return [
+            "id" => "required|int",
+            "title" => "required|max:120",
+            "body" => "required|min:12",
+            "slug_id" => "required",
+            "slug" => "request"
+        ];
+    }
+
+    #[ArrayShape([
+        "*.required" => "string",
+        "title.max" => "string",
+        "body.min" => "string"
+    ])] public function messages(): array
+    {
+        return [
+            "*.required" => ":attribute 를 작성해주세요.",
+            "title.max" => "게시글 제목은 최대 120자입니다.",
+            "body.min" => "게시글 내용은 최소 12자입니다.",
+        ];
+    }
+
+    /**
+     * 요청 들어온 데이터 초기 값 추가
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $slug_title = Str::slug(Str::of($this['title'])->trim(), '-');
+        $this->merge([
+            "slug_id" => $this['slug_id'],
+            "slug" => $this['slug_id']. "-". $slug_title,
+        ]);
+    }
+}
---
+++ week_1/app/Http/Requests/Board/BoardWriteRequest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Requests\Board;
+
+use App\Models\Board;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+use JetBrains\PhpStorm\ArrayShape;
+
+class BoardWriteRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    #[ArrayShape([
+        "title" => "string",
+        "body" => "string",
+        "slug_id" => "string",
+        "slug" => "string"
+    ])] public function rules(): array
+    {
+        return [
+            "title" => "required|max:120",
+            "body" => "required|min:12",
+            "slug_id" => "required",
+            "slug" => "required"
+        ];
+    }
+
+    #[ArrayShape([
+        "*.required" => "string",
+        "title.max" => "string",
+        "body.min" => "string"
+    ])] public function messages(): array
+    {
+        return [
+            "*.required" => ":attribute 를 작성해주세요.",
+            "title.max" => "게시글 제목은 최대 120자입니다.",
+            "body.min" => "게시글 내용은 최소 12자입니다.",
+        ];
+    }
+
+    /**
+     * 요청 들어온 데이터 초기 값 추가
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $new_slug_id = $this->getUniqueSlugId();
+        $slug_title = Str::slug(Str::of($this['title'])->trim(), '-');
+        $this->merge([
+            "slug_id" => $new_slug_id,
+            "slug" => $new_slug_id. "-". $slug_title,
+        ]);
+    }
+
+    /**
+     * 유니크한 Slug의 ID가 없을 경우 생성
+     * @return int
+     */
+    private function getUniqueSlugId(): int
+    {
+        $slug_id = mt_rand(100000000, 9999999999);
+        $slug = Board::where("slug_id", $slug_id)->count();
+        if ($slug !== 0) {
+            $this->getUniqueSlugId();
+        }
+
+        return $slug_id;
+    }
+}
