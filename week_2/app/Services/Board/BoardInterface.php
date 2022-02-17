3c8f373d03330adc4ee4a6b54eb864e006caaaf5
Kyungseo-Park <troublesome.dev@gmail.com>
2022-02-18 02:06:15 +0900

    feat(DIP): BoardService 테스트 코드 작성(게시글 조회)

--- week_2/app/Services/Board/BoardInterface.php
+++ week_2/app/Services/Board/BoardInterface.php
@@ -2,9 +2,18 @@
 
 namespace App\Services\Board;
 
+use Illuminate\Database\Eloquent\Model;
+
 interface BoardInterface
 {
+    /**
+     * @return mixed
+     */
     public function listView();
+
+    /**
+     * @return mixed
+     */
     public function detailView();
     public function editView();
     public function create();
