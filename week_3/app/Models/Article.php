53ad18db2831145751d6797bf89de59092024b6b
Kyungseo-Park <troublesome.dev@gmail.com>
2022-02-27 21:27:27 +0900

    모델 추가

---
+++ week_3/app/Models/Article.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+/**
+ * App\Models\Article
+ *
+ * @property int $id
+ * @property string $slug_id URL Slug
+ * @property string $slug Slug
+ * @property string $title 제목
+ * @property string $content 내용
+ * @property int $is_show 공개 여부
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\ArticleFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Article newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Article query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereAdminId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereCategory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereIsShow($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Article whereUrl($value)
+ * @mixin \Eloquent
+ */
+class Article extends Model
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'id',
+        'slug_id',
+        'slug',
+        'title',
+        'content',
+        'is_show',
+        'created_at',
+        'updated_at',
+    ];
+}
