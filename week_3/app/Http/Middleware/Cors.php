993ffab9aa7a1aacdebeafb42efe4e5ae2c07ce6
Kyungseo-Park <troublesome.dev@gmail.com>
2022-02-27 21:32:05 +0900

    CORS 이슈에 대한 미들웨어 추가

---
+++ week_3/app/Http/Middleware/Cors.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next): mixed
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+    }
+}
