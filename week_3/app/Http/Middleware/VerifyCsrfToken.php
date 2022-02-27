1ddfc44a333f6cfac463184aad45c5f308a961d6
Kyungseo-Park <troublesome.dev@gmail.com>
2022-02-27 22:16:27 +0900

    CSRF 호스트 추가

--- week_3/app/Http/Middleware/VerifyCsrfToken.php
+++ week_3/app/Http/Middleware/VerifyCsrfToken.php
@@ -8,10 +8,10 @@ class VerifyCsrfToken extends Middleware
 {
     /**
      * The URIs that should be excluded from CSRF verification.
-     *
+     * CSRF 사용 하려면 여기에 도메인을 추가 해야할듯합니다.
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'http://localhost:3000/*',
     ];
 }
