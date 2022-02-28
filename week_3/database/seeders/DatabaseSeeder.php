474e4d7d7a9e10617050f8c17a04bdc2616f2b3d
Kyungseo-Park <troublesome.dev@gmail.com>
2022-02-28 21:18:55 +0900

    데이터베이스 씨드 추가

--- week_3/database/seeders/DatabaseSeeder.php
+++ week_3/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Factory::create();
+        foreach (range(1,1000) as $index) {
+            $title = $faker->realText($faker->numberBetween(10,20));
+            Article::create([
+                'slug_id' => Str::uuid(),
+                'slug' => strtolower(preg_replace('/[^a-zA-Z가-힣0-9]+/', '-', $title)),
+                'title' => $title,
+                'content' =>$faker->realText($faker->numberBetween(100,200)),
+            ]);
+        }
         // \App\Models\User::factory(10)->create();
     }
 }
