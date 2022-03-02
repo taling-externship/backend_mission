b9294096bbf31599ef80b2913a808f4e70ad6e9e
Kyungseo-Park <troublesome.dev@gmail.com>
2022-03-03 00:21:25 +0900

    4주차 과제 세팅 3주차 => 4주차 복사

---
+++ week_4/routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Article
+Route::get('articles',                   [\App\Http\Controllers\ArticleController::class, 'getArticles']);      // 전체 조회
+Route::post('articles',                  [\App\Http\Controllers\ArticleController::class, 'addArticle']);       // 특정 데이터 추가
+Route::put('articles',           [\App\Http\Controllers\ArticleController::class, 'update']);            // 데이터 업로드
+Route::delete('articles/{id}',           [\App\Http\Controllers\ArticleController::class, 'delete']);          // 데이터 삭제
+Route::get('articles/{slug_id}/{slug}', [\App\Http\Controllers\ArticleController::class, 'getArticle']);       // 특정 데이터 조회
