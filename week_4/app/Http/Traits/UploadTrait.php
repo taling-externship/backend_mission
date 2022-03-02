75bffe39fe66b9471f0c68470466df2242dbc240
Kyungseo-Park <troublesome.dev@gmail.com>
2022-03-03 01:59:33 +0900

    파일 업로드에 대한 Trait 추가

--- week_4/app/Http/Traits/UploadTrait.php
+++ week_4/app/Http/Traits/UploadTrait.php
@@ -2,7 +2,19 @@
 
 namespace App\Http\Traits;
 
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
+
 trait UploadTrait
 {
-
+    public function uploadFile(UploadedFile $uploadedFile,
+                                            $folder = null,
+                                            $disk = 'public',
+                                            $filename = null): bool|string
+    {
+        if (is_null($filename)) {
+            $filename = Str::random(25);
+        }
+        return $uploadedFile->storeAs($folder, $filename.'.'.$uploadedFile->getClientOriginalExtension(), $disk);
+    }
 }
