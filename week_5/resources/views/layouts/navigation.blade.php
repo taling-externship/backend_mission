1dc1b27ca302eea55de62198c020307222a75860
sunday00 <kind65505256@gmail.com>
2022-03-09 04:12:15 +0900

    j

--- week_5/resources/views/layouts/navigation.blade.php
+++ week_5/resources/views/layouts/navigation.blade.php
@@ -5,13 +5,18 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('article.index') }}">
+                    <a href="{{ route('article.index') }}" class="flex">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                         <h1>LARAVEL APP</h1>
                     </a>
                 </div>
 
-
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('article.index')" :active="request()->routeIs('Articles')">
+                        {{ __('Articles') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
