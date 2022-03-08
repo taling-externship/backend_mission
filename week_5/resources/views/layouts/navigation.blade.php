5539e7fa48bc5dbc6e78596073c6f2663dec7c77
sunday00 <kind65505256@gmail.com>
2022-03-09 02:13:01 +0900

    j

--- week_5/resources/views/layouts/navigation.blade.php
+++ week_5/resources/views/layouts/navigation.blade.php
@@ -7,6 +7,7 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('article.index') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <h1>LARAVEL APP</h1>
                     </a>
                 </div>
 
