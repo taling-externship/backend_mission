1a04ae5bc4d82ca8ba8f85745b4ae0201a7dc45b
sunday00 <kind65505256@gmail.com>
2022-03-09 02:48:06 +0900

    j

--- week_5/resources/views/layouts/navigation.blade.php
+++ week_5/resources/views/layouts/navigation.blade.php
@@ -11,12 +11,7 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('article.index')" :active="request()->routeIs('Articles')">
-                        {{ __('Articles') }}
-                    </x-nav-link>
-                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
