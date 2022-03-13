d459b19df63afe8ae094af1be69cd10dbe0f91cd
Kyungseo-Park <troublesome.dev@gmail.com>
2022-03-13 21:57:28 +0900

    feat(test-스케줄링): 스케줄링 동작 테스트

--- week_5/app/Console/Kernel.php
+++ week_5/app/Console/Kernel.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        // 작업스케줄링을 위해 추가함.
+        Commands\AuthSandMailCron::class,
+    ];
     /**
      * Define the application's command schedule.
      *
@@ -16,6 +21,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('authSandMail:cron ')->everyMinute();
     }
 
     /**
